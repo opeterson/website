434990d7abc68e49b92a4407e65e4d62e511a144
Owen Peterson <owen.a.peterson@gmail.com>
2018-12-09 09:39:53 -0600

    Added HomeControllerTest class and test methods.

---
+++ website/src/test/java/ca/owenpeterson/website/controllers/HomeControllerTest.java
@@ -0,0 +1,35 @@
+package ca.owenpeterson.website.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import ca.owenpeterson.website.models.HomeModel;
+
+public class HomeControllerTest 
+{
+	private HomeController homeController = new HomeController();
+	
+	@Test
+	public void testGetHome()
+	{
+		ModelAndView result = homeController.getHome();
+		assertNotNull(result);
+		assertEquals("home/home.jsp", result.getViewName());
+	}
+	
+	@Test
+	public void testGetHomeModel()
+	{
+		HomeModel model = homeController.getHomeModel();
+		assertNotNull(model);
+		assertNotNull(model.getDate());
+		LocalDate today = LocalDate.now();
+		LocalDate date = model.getDate().toLocalDate();
+		assertEquals(today, date);		
+	}
+}

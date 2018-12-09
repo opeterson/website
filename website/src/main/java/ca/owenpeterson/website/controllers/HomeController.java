763bfd397c483dd37e3ae16c9d0c1eaabfeecad9
Owen <Owen@WIN10-DESKTOP.hitronhub.home>
2018-12-09 07:32:42 -0600

    Added log4j configuration and some debug statements.

--- website/src/main/java/ca/owenpeterson/website/controllers/HomeController.java
+++ website/src/main/java/ca/owenpeterson/website/controllers/HomeController.java
@@ -2,6 +2,8 @@ package ca.owenpeterson.website.controllers;
 
 import java.time.LocalDateTime;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,19 +14,25 @@ import ca.owenpeterson.website.models.HomeModel;
 @Controller
 public class HomeController 
 {
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
+	
 	@ModelAttribute("homeModel")
 	public HomeModel getHomeModel()
 	{
+		LOGGER.debug("Creating model for home page.");
 		HomeModel model = new HomeModel();
 		LocalDateTime today = LocalDateTime.now();
 		model.setDate(today);
+		LOGGER.debug("Returning model for home page.");
 		return model;
 	}		
 	
 	@GetMapping("/")
 	public ModelAndView getHome()
 	{
+		LOGGER.debug("Handling request for home page.");
 		ModelAndView view = new ModelAndView("home/home.jsp");
+		LOGGER.debug("Returning view for home page.");
 		return view;
 	}
 }
